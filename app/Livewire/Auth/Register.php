03e9bc9fa890cd8d4724ad0093d7b50a6afbf7ca
Guilherme Haynes Howe <zerossb@gmail.com>
2025-04-02 21:00:49 -0300

    Add validation rules to Register component and enhance registration tests

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -8,18 +8,29 @@ use Livewire\Component;
 
 class Register extends Component
 {
-    public ?string $name;
-    public ?string $email;
-    public ?string $email_confirmation;
-    public ?string $password;
+    public ?string $name = null;
+    public ?string $email = null;
+    public ?string $email_confirmation = null;
+    public ?string $password = null;
 
     public function render(): View
     {
         return view('livewire.auth.register');
     }
 
+    protected function rules(): array
+    {
+        return [
+            'name' => ['required', 'max:255'],
+            'email' => ['required', 'max:255', 'email', 'confirmed'],
+            'password' => ['required', 'max:255'],
+        ];
+    }
+
     public function register(): void
     {
+        $this->validate();
+
         User::query()->create([
             'name' => $this->name,
             'email' => $this->email,
--- tests/Feature/Livewire/Auth/RegisterTest.php
+++ tests/Feature/Livewire/Auth/RegisterTest.php
@@ -26,3 +26,22 @@ it('should be able to register a new user in the system', function () {
 
     assertDatabaseCount('users', 1);
 });
+
+it('validation rules', function ($testCase) {
+    Livewire::test(Register::class)
+        ->set($testCase->field, $testCase->value)
+        ->call('register')
+        ->assertHasErrors([
+            $testCase->field => $testCase->rule,
+        ]);
+})
+    ->with([
+        'name::required' => (object)['field' => 'name', 'value' => '', 'rule' => 'required'],
+        'name::max:255' => (object)['field' => 'name', 'value' => str_repeat('*', 256), 'rule' => 'max'],
+        'email::required' => (object)['field' => 'email', 'value' => '', 'rule' => 'required'],
+        'email::email' => (object)['field' => 'email', 'value' => 'not-an-email', 'rule' => 'email'],
+        'email::max:255' => (object)['field' => 'email', 'value' => str_repeat('*', 256) . '@dow.com', 'rule' => 'max'],
+        'email::confirmed' => (object)['field' => 'email', 'value' => 'john@doe.com', 'rule' => 'confirmed'],
+        'password::required' => (object)['field' => 'password', 'value' => '', 'rule' => 'required'],
+        'password::max:255' => (object)['field' => 'password', 'value' => str_repeat('*', 256), 'rule' => 'max'],
+    ]);
